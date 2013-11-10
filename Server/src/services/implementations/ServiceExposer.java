53a6553be98cc331164d891a697c4c7ef7f7b98c
root <root@server-one>
2013-11-10 02:41:26 +0200

    Forging skeleton.

--- Server/src/services/implementations/ServiceExposer.java
+++ Server/src/services/implementations/ServiceExposer.java
@@ -3,13 +3,34 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package services.implementations;
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import services.BindingConsts;
+import services.server.RegionsDAIF;
 
 /**
  *
  * @author root
  */
 public class ServiceExposer {
-    
+
+    private static boolean done = false;
+
+    public static void exposeAll() throws Exception {
+        
+        if (done) { //bootstrap only once
+            return;
+        }
+        
+        Registry registry = LocateRegistry.createRegistry(BindingConsts.port);
+
+        RegionsDAIF regionsIF = new RegionDAImpl(); //local implementation
+        RegionsDAIF regionsStub = (RegionsDAIF) UnicastRemoteObject.exportObject(regionsIF, 0); //expose
+        registry.bind(BindingConsts.regionDA, regionsStub); //publish to the registry
+
+        done = true; //bootstrapping went well
+    }
 }
