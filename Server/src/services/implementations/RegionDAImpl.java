fe776c791972496a8d71e4a51805885e7f2fc85b
root <root@server-one>
2013-11-10 02:21:53 +0200

    Forging skeleton.

---
+++ Server/src/services/implementations/RegionDAImpl.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package services.implementations;
+
+import dto.Result;
+import dto.domain.Region;
+import dto.domain.User;
+import dto.filters.RegionFilter;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.List;
+import services.server.RegionsDAIF;
+
+/**
+ *
+ * @author root
+ */
+public class RegionDAImpl implements RegionsDAIF {
+
+    @Override
+    public Result<Region> insertRegion(List<Region> ins, User user) throws RemoteException {
+        List<Region> res = new ArrayList<Region>();
+        Exception ex = null;
+        try {
+            System.out.println("inserting");
+            //TODO
+        } catch (Exception exc) {
+            ex = exc;
+        }
+        return new Result(res, ex);
+    }
+
+    @Override
+    public Result<Region> updateRegion(List<Region> upd, User user) throws RemoteException {
+        List<Region> res = new ArrayList<Region>();
+        Exception ex = null;
+        try {
+            System.out.println("updating");
+            //TODO
+        } catch (Exception exc) {
+            ex = exc;
+        }
+        return new Result(res, ex);
+    }
+
+    @Override
+    public Result<Region> deleteRegion(List<Region> del, User user) throws RemoteException {
+        List<Region> res = new ArrayList<Region>();
+        Exception ex = null;
+        try {
+            System.out.println("deleting");
+            //TODO
+        } catch (Exception exc) {
+            ex = exc;
+        }
+        return new Result(res, ex);
+    }
+
+    @Override
+    public Result<Region> fetchRegions(RegionFilter filter) {
+        List<Region> res = new ArrayList<Region>();
+        Exception ex = null;
+        try {
+            System.out.println("fetching");
+            //TODO
+        } catch (Exception exc) {
+            ex = exc;
+        }
+        return new Result(res, ex);
+    }
+
+}
