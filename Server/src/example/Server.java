25a120f3540b5b8e39079565b6b5fc814e0a62b8
root <root@server-one>
2013-11-10 02:43:56 +0200

    Forging skeleton.

--- Server/src/example/Server.java
+++ Server/src/example/Server.java
@@ -11,6 +11,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import services.implementations.ServiceExposer;
 
 /**
  *
@@ -20,6 +21,14 @@ public class Server extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
+        
+        try {
+            ServiceExposer.exposeAll();
+        } catch(Exception e){
+            e.printStackTrace();
+            return;
+        }
+        
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
