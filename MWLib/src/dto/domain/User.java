e87fafbe0d1a8394c90761384bc237f958fc4a5f
root <root@server-one>
2013-11-13 23:56:58 +0200

    Some tests

--- MWLib/src/dto/domain/User.java
+++ MWLib/src/dto/domain/User.java
@@ -6,6 +6,7 @@
 
 package dto.domain;
 
+import dto.rolemanagement.Role;
 import java.util.Date;
 
 /**
@@ -13,10 +14,44 @@ import java.util.Date;
  * @author root
  */
 public class User extends PersistedDTO{
-
+    
+    public String fName;
+    public String sName;
+    public String lName;
+    public String eMail;
+    public String password;
+    public String description;
+    public Role role;
+    
     public User(int ID, Date timeIns, Date timeUpd, User userUpd) {
         super(ID, timeIns, timeUpd, null, userUpd);
     }
     
+    public String getFName(){
+        return fName;
+    }
     
+    public String getSName(){
+        return sName;
+    }
+    
+    public String getLName(){
+        return lName;
+    }
+    
+    public String getEMail(){
+        return eMail;
+    }
+    
+    public String getPassword(){
+        return password;
+    }
+    
+    public String getDescription(){
+        return description;
+    }
+    
+    public Role getRole(){
+        return role;
+    }
 }
