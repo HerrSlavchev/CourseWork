7dbe04fda07c231c1baccb12d1515b84524755a4
root <root@server-one>
2013-12-11 23:42:24 +0200

    Some tests

--- Client/src/example/Client.java
+++ Client/src/example/Client.java
@@ -12,11 +12,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import services.BindingConsts;
 import services.RemoteServices;
-import services.client.NotifiableIF;
-import services.server.ClientManagerIF;
-import static session.Session.notifImpl;
+
 
 /**
  *
