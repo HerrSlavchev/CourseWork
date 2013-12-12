0302515b38dd4bda3541e56622264d8d425ae891
root <root@server-one>
2013-12-13 01:38:59 +0200

    Some tests

--- Server/src/services/implementations/UserDAImpl.java
+++ Server/src/services/implementations/UserDAImpl.java
@@ -9,6 +9,7 @@ import dao.ConnectionProvider;
 import dto.Result;
 import dto.domain.User;
 import dto.filters.UserFilter;
+import dto.rolemanagement.Role;
 import java.rmi.RemoteException;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -38,7 +39,7 @@ public class UserDAImpl implements UserDAIF {
 
         try {
             Connection conn = ConnectionProvider.getConnection();
-            String slct = "SELECT password, hash, iterations FROM user WHERE e_mail = '" + email + "';";
+            String slct = "SELECT ID, password, hash, iterations FROM user WHERE e_mail = '" + email + "';";
             Statement stmt = conn.createStatement();
             ResultSet rs = stmt.executeQuery(slct);
 
@@ -48,6 +49,7 @@ public class UserDAImpl implements UserDAIF {
                 exc = new Exception("More than one users are registered with this e-mail.");
             } else {
                 rs.next();
+                int ID = rs.getInt("ID");
                 String realPassword = rs.getString("password");
                 String realSalt = rs.getString("salt");
                 int realIterations = rs.getInt("iterations");
@@ -56,7 +58,24 @@ public class UserDAImpl implements UserDAIF {
                 if (false == valid) {
                     exc = new Exception("Wrong e-mail or password.");
                 } else {
-                    //SELECT USER AND ROLE, ATTACH CLIENT TO THE CLIENTMANAGER
+                    User u = new User(ID, null, null, null);
+                    slct = "select " + 
+                                "u.f_name, " + 
+                                "u.l_name, " +
+                                "u.e_mail, " +
+                                "u.role " +
+                            "from " +
+                                "user u " +
+                            "where " +
+                                "u.ID = " + ID + ";";
+                    rs = stmt.executeQuery(slct);
+                    rs.next();
+                    u.fName = rs.getString("u.f_name");
+                    u.lName = rs.getString("u.l_name");
+                    u.eMail = rs.getString("u.e_mail");
+                    int roleIdx = rs.getInt("u.role");
+                    u.role = Role.values()[roleIdx];
+                    lst.add(u);
                 }
 
             }
@@ -65,8 +84,12 @@ public class UserDAImpl implements UserDAIF {
         } catch (Exception e) {
             exc = e;
         }
-
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        if(true) {
+            throw new UnsupportedOperationException("MUST ATTACH TO CLIENTMANAGER!");
+        }
+        //ATTACH CLIENT TO CLIENTMANAGER!
+        return new Result(lst, exc);
     }
 
     @Override
