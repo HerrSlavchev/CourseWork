90718e6c6faf49198748457218fea807a2f67688
root <root@server-one>
2013-12-17 13:26:10 +0200

    Batch checking.

--- Server/src/dao/DAOUtils.java
+++ Server/src/dao/DAOUtils.java
@@ -19,7 +19,7 @@ import session.SessionCodeProviderIF;
 public class DAOUtils {
 
     public static final SessionCodeProviderIF sessionCodeProvider = BasicSessionCodeProvider.getInstance();
-    
+    public static final int MAX_BATCH_SIZE = 1000;
     
     public static void releaseRes(Connection conn, Statement stmt, ResultSet rs) throws SQLException {
         if (rs != null && false == rs.isClosed()) {
@@ -29,11 +29,25 @@ public class DAOUtils {
             stmt.close();
         }
         if (conn != null && false == conn.isClosed()) {
+            conn.setAutoCommit(true);
             conn.close();
         }
     }
     
     public static void processSQLException(SQLException SQLe){
         SQLe.printStackTrace();
+    }
+    
+    public static boolean processBatchRes(int[] batchRes){
+        
+        if (batchRes.length == 0){
+            return false;
+        }
+        for (int i : batchRes){
+            if (i == Statement.EXECUTE_FAILED) {
+                return false;
+            }
+        }
+        return true;
     }
 }
