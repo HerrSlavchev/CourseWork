f57007cc4db2c1711b58fe5114f9f0f07e97d9d8
root <root@server-one>
2014-01-02 21:49:27 +0200

    Fixed binding.

--- MWLib/src/services/BindingConsts.java
+++ MWLib/src/services/BindingConsts.java
@@ -21,8 +21,8 @@ public class BindingConsts {
     //===CLIENT METHODS===
         
     //===SERVER METHODS===
-    public static final String GATEGORY_DA = "CategoryDA";
-    public static final String SUBGATEGORY_DA = "SubCategoryDA";
+    public static final String CATEGORY_DA = "CategoryDA";
+    public static final String SUBCATEGORY_DA = "SubCategoryDA";
     public static final String REGION_DA = "RegionDA";
     public static final String TOWN_DA = "TownDA";
     public static final String CLIENT_MANAGER = "ClientManager";
